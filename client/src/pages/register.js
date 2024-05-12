ab251514be6c4cfe318bf3eac12069137996232c
saileshman <saileshmankunwar2021@gmail.com>
2024-05-12 21:29:15 +0545

    hello

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,27 +1,45 @@
-import { NavLink } from "react-router-dom";
-
+//import { NavLink } from "react-router-dom";
+import {useState} from "react";
+import axios from "axios";
 
 function Register(){
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // clear the written after submit
+
+  const handleClick = async(e)=>{
+    e.preventDefault();
+    try{
+      const res = await axios.post(`${process.env.API}/api/v1/auth/register`,(name,email,password))
+    } catch(error){
+      alert.error("something is wrong")
+    }
+    console.log(name,email,password)
+    alert("register successful !")
+  }
+
 
     return <>
-  <form className="row g-3 needs-validation" noValidate>
+  <form className="row g-3 needs-validation" onSubmit={handleClick} noValidate>
   <div className="col-md-4">
     <label htmlFor="validationCustom01" className="form-label">Full name</label>
-    <input type="text" className="form-control"  required />
+    <input type="text" className="form-control"  value={name}  onChange={(e)=> setName(e.target.value)} required />
     <div className="valid-feedback">
       Looks good!
     </div>
   </div>
   <div className="col-md-4">
     <label htmlFor="validationCustom02" className="form-label">Email</label>
-    <input type="text" className="form-control"  required />
+    <input type="text" className="form-control" value={email} onChange={(e)=> setEmail(e.target.value)} required />
     <div className="valid-feedback">
       Looks good!
     </div>
   </div>
   <div className="form-floating">
-    <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+    <input type="password" className="form-control" id="floatingPassword" placeholder="Password" value={password}  onChange={(e)=> setPassword(e.target.value)} />
     <label htmlFor="floatingPassword">Password</label>
   </div>
 
@@ -36,9 +54,9 @@ function Register(){
       </div>
     </div>
   </div>
-  <NavLink to="/login">
+  {/* <NavLink to="/login"> */}
     <button className="btn btn-primary" type="submit">Submit form</button>
-    </NavLink>
+    {/* </NavLink> */}
  
 </form>
 
